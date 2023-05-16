09d8dd3ae5d82ff793842b1bb3d55dc3c47e8a0d
IsYancem <122916029+IsYancem@users.noreply.github.com>
2023-05-16 18:45:58 -0500

    Add Logic

---
+++ task_master_flutter/lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Color.fromARGB(255, 109, 188, 121);
+
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Color.fromARGB(255, 30, 27, 27);
+
+const Color tdBGColor = Color.fromARGB(255, 128, 227, 171);
